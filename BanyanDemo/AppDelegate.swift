bbd42b19c03b78487ddbd06465581fdf1884d365
Shawn Frank <shawn@mindhyve.com>
2021-02-16 12:51:52 +0400

    initial commit with starter project

---
+++ BanyanDemo/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  BanyanDemo
+//
+//  Created by Shawn Frank on 16/02/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navController = UINavigationController()
+        navController.isNavigationBarHidden = true
+        
+        let rootVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "dashboardVC")
+        navController.setViewControllers([rootVC], animated: true)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ BanyanDemo/DashboardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DashboardViewController.swift
+//  BanyanDemo
+//
+//  Created by Shawn Frank on 16/02/2021.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
