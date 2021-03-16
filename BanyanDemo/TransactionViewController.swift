8a983a7bcb85b160caa5163d2812cdcabc3f92a5
Shawn Frank <shawn@mindhyve.com>
2021-03-16 18:03:15 +0400

    #9 - add pull to refresh

--- BanyanDemo/TransactionViewController.swift
+++ BanyanDemo/TransactionViewController.swift
@@ -7,14 +7,57 @@
 
 import UIKit
 
+enum AWSEnvironment {
+  case development
+  case laboratory
+}
+
 class TransactionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
   @IBOutlet weak var transactionTableView: UITableView!
+  @IBOutlet weak var environmentSegment: UISegmentedControl!
+  let refreshControl = UIRefreshControl()
+  private var awsEnvironment = AWSEnvironment.development
+  private var cognitoId = "us-east-1:b244ca7b-9f4f-45fd-b3d1-fda5bbaaa36d"
   
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    setUpSegmentView()
+    setUpTableView()
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    simulateTablePull()
+  }
+  
+  private func setUpSegmentView() {
+    environmentSegment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black],
+                                              for: .selected)
+    environmentSegment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black],
+                                              for: .normal)
+  }
+  
+  private func setUpTableView() {
     transactionTableView.backgroundColor = .white
     transactionTableView.tableFooterView = UIView()
+    transactionTableView.refreshControl = refreshControl
+    transactionTableView.refreshControl?.tintColor = .darkGray
+    transactionTableView.refreshControl?.addTarget(self,
+                                                   action: #selector(didPullToRefresh),
+                                                   for: .valueChanged)
+  }
+  
+  private func getTransactionData() {
+    didPullToRefresh()
+  }
+  
+  @objc
+  private func didPullToRefresh() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      self.transactionTableView.refreshControl?.endRefreshing()
+    }
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -25,5 +68,27 @@ class TransactionViewController: UIViewController, UITableViewDataSource, UITabl
     let cell = tableView.dequeueReusableCell(withIdentifier: TransactionTableViewCell.identifier) as! TransactionTableViewCell
     return cell
   }
+  
+  @IBAction func segmentChangedAction(_ sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+      awsEnvironment = .development
+      cognitoId = "us-east-1:b244ca7b-9f4f-45fd-b3d1-fda5bbaaa36d"
+    }
+    else {
+      awsEnvironment = .laboratory
+      cognitoId = "us-east-1:602956b1-c5e3-41b3-9bdf-a87bce26feb6"
+    }
 
+    simulateTablePull()
+  }
+  
+  private func simulateTablePull() {
+    DispatchQueue.main.asyncAfter(deadline: .now()) {
+      self.transactionTableView.setContentOffset(CGPoint(x: 0,
+                                                         y: self.transactionTableView.contentOffset.y - (self.refreshControl.frame.size.height)),
+                                                         animated: true)
+      self.transactionTableView.refreshControl?.beginRefreshing()
+      self.getTransactionData()
+    }
+  }
 }
